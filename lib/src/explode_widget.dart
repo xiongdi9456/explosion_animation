d26267677db78fdf46dc5006ba747aaed68ce602
sheldon <xiongdi9456@gmail.com>
2020-12-26 15:46:02 +0900

    update

--- lib/src/explode_widget.dart
+++ lib/src/explode_widget.dart
@@ -1,6 +1,5 @@
 import 'package:explosion_animation/src/drop_path.dart';
 import 'package:explosion_animation/src/painter.dart';
-import 'package:explosion_animation/src/path_mixin.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 import 'package:vector_math/vector_math_64.dart';
@@ -32,33 +31,26 @@ class _ExplodeState extends State<Explode> with SingleTickerProviderStateMixin {
   AnimationController _controller;
   Animation<double> _explodeAnimation;
   Animation<double> _shakeAnimation;
-  static const List<int> signs = [-1,1];
+  // static const List<int> signs = [-1,1];
   math.Random random = new math.Random();
   final GlobalKey renderKey = GlobalKey();
   Size size;
-  static const int particleCount = 300;
+  static const int particleCount = 10;
   List<DropPath> particles = [];
   @override
   void initState() {
     _controller = AnimationController(
       vsync: this,
       duration: widget.shakeBeforeExploding
-          ? Duration(
-              seconds: 1)
+          ? Duration(seconds: 1)
           : widget.explodeDuration,
     );
     _explodeAnimation = Tween<double>(begin: 0.0, end: 10.0).animate(
-        CurvedAnimation(
-            parent: _controller,
-            curve: Interval(
-                0.3,
-                1.0)));
+        CurvedAnimation(parent: _controller, curve: Interval(0.3, 1.0)));
     _shakeAnimation = widget.shakeBeforeExploding
         ? Tween<double>(begin: 0.0, end: 100.0).animate(CurvedAnimation(
             parent: _controller,
-            curve: Interval(
-                0.0, 0.3,
-                curve: Curves.easeInQuint)))
+            curve: Interval(0.0, 0.3, curve: Curves.easeInQuint)))
         : null;
     _controller.addListener(() {
       setState(() {});
@@ -71,29 +63,36 @@ class _ExplodeState extends State<Explode> with SingleTickerProviderStateMixin {
       setState(() {
         size = renderKey.currentContext.findRenderObject().paintBounds.size;
         createParticles();
-        if(!widget.shakeBeforeExploding) _controller.forward();
+        if (!widget.shakeBeforeExploding) _controller.forward();
       });
     });
     Future.delayed(Duration(seconds: 2)).then((_) {
-      if (widget.instantExplode && widget.shakeBeforeExploding) _controller.forward();
+      if (widget.instantExplode && widget.shakeBeforeExploding)
+        _controller.forward();
     });
     super.initState();
   }
-  createParticles(){
-    for(int i =0; i< particleCount;i++){
+
+  createParticles() {
+    for (int i = 0; i < particleCount; i++) {
       particles.add(DropPath(
-        initialPosition: Offset(random.nextInt(size.width.toInt()).toDouble(),random.nextInt(size.height.toInt()).toDouble()),
-        initialRadius: random.nextDouble()*12,
-        center: Offset(size.width/2,size.height/2)
-      ));
+          initialPosition: Offset(random.nextInt(size.width.toInt()).toDouble(),
+              random.nextInt(size.height.toInt()).toDouble()),
+          initialRadius: random.nextDouble() * 2,
+          center: Offset(size.width / 2, size.height / 2)));
     }
   }
+
   Vector3 shake() {
     print(_shakeAnimation.status);
-    if (_controller.value>=0.3) return Vector3.all(0);
-    double x = widget.shakeDistance * random.nextDouble()*(random.nextBool()?-1:1);
-    double y = widget.shakeDistance * random.nextDouble()*(random.nextBool()?-1:1);
-    print(Size(x,y));
+    if (_controller.value >= 0.3) return Vector3.all(0);
+    double x = widget.shakeDistance *
+        random.nextDouble() *
+        (random.nextBool() ? -1 : 1);
+    double y = widget.shakeDistance *
+        random.nextDouble() *
+        (random.nextBool() ? -1 : 1);
+    print(Size(x, y));
     return Vector3(x, y, 0);
   }
 
@@ -106,10 +105,8 @@ class _ExplodeState extends State<Explode> with SingleTickerProviderStateMixin {
       child: CustomPaint(
           size: size ?? Size.zero,
           painter: ParticlesPainter(
-            dropPaths: particles,
-            t: _explodeAnimation.value
-          ),
-          child: _explodeAnimation.value==0 ? widget.child : null),
+              dropPaths: particles, t: _explodeAnimation.value),
+          child: _explodeAnimation.value == 0 ? widget.child : null),
     );
   }
 }
